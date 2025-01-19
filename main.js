e87475e4bbd5626d60153e2e615b3d30896e3376
gold <souissi13mouldi@gmail.com>
2025-01-19 02:25:17 -0500

    form added

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
+// close the menu on outside click
+document.addEventListener("click", function (event) {
+  // Select all details elements
+  var detailsElements = document.querySelectorAll("details");
 
+  // Iterate over each details element
+  detailsElements.forEach(function (details) {
+    var summary = details.querySelector("summary");
+
+    // Check if the clicked target is inside the details element or is the summary itself
+    if (!details.contains(event.target) && event.target !== summary) {
+      details.removeAttribute("open"); // Close the details
+    }
+  });
+});
